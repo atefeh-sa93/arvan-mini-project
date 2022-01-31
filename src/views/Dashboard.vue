40118b5392f6162184b7d7b827f040d780fbf492
Atefeh <atefeh@buzzjective.com>
2022-01-31 13:17:55 +0330

    create a dashbaord page

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-main>
+    <Navbar :user-info="authenticated" />
+  </v-main>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Navbar from "../components/Navbar.vue";
+export default {
+  name: "Dashboard",
+
+  components: { Navbar },
+
+  computed: {
+    ...mapGetters({
+      authenticated: 'auth/authenticated',
+    }),
+  },
+};
+</script>
