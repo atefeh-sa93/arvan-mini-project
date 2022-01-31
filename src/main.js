52145082058f46ac0295fbe0f2e85d62571dd21d
Atefeh <atefeh@buzzjective.com>
2022-01-31 13:17:13 +0330

    add headers of the axios and fix lint

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-import vuetify from './plugins/vuetify'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import axios from "axios";
 
 Vue.config.productionTip = false;
+
+axios.defaults.baseURL = 'https://api.realworld.io/api/';
+axios.defaults.headers.post['X-Requested-With'] = 'XMLHttpRequest';
+axios.defaults.headers.post['Content-Type'] = 'application/json';
 
 new Vue({
   router,
   store,
   vuetify,
-  render: (h) => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
