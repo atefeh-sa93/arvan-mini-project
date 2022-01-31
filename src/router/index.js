28fd1491d4022597d2bfeecd676d2edc3d7c92f8
Atefeh <atefeh@buzzjective.com>
2022-01-31 13:18:25 +0330

    add login/register routes and add middleWare

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,42 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Dashboard from '../views/Dashboard.vue';
 import About from '../views/About.vue';
+import Login from '../views/Auth/Login.vue';
+import Register from '../views/Auth/Register.vue';
+import store from '@/store';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: Dashboard,
+    beforeEnter: (to, from, next) => {
+      if(!store.getters['auth/authenticated']) {
+        return next({
+          name: 'Login'
+        })
+      }
+      next();
+
+    }
   },
   {
     path: '/about',
     name: 'About',
     component: About,
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
   },
 ];
 
